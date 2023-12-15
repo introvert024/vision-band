ff5ab2859b7ae86750e8ac33f505ab011c2bcdea
Introvert <krishv594@gmail.com>
2023-12-16 00:58:28 +0530

    Update obnolivepreview.py

--- Object_detection/obnolivepreview.py
+++ Object_detection/obnolivepreview.py
@@ -1,22 +1,20 @@
 # import packages
-from imutils.video import VideoStream
-from imutils.video import FPS
 import numpy as np
 import argparse
-import imutils
-import time
 import cv2
+import time
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-p", "--prototxt", required=True,
-	help="path to Caffe 'deploy' prototxt file")
+    help="path to Caffe 'deploy' prototxt file")
 ap.add_argument("-m", "--model", required=True,
-	help="path to Caffe pre-trained model")
+    help="path to Caffe pre-trained model")
 ap.add_argument("-c", "--confidence", type=float, default=0.2,
-	help="minimum probability to filter weak predictions")
+    help="minimum probability to filter weak predictions")
 args = vars(ap.parse_args())
 
+# Define the class labels (modify as needed)
 CLASSES = ["aeroplane", "background", "bicycle", "bird", "boat",
            "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
            "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
@@ -25,63 +23,46 @@ CLASSES = ["aeroplane", "background", "bicycle", "bird", "boat",
 # Assigning random colors to each of the classes
 COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 
-
+# load our serialized model
 print("[INFO] loading model...")
 net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 
-
-
-print("[INFO] starting video stream...")
-vs = VideoStream(src=0).start()
-
+# initialize the video stream
+vs = cv2.VideoCapture(0)
 time.sleep(2.0)
 
-fps = FPS().start()
-
-
+# continuous loop for object detection
 while True:
-	
-	frame = vs.read()
-	frame = imutils.resize(frame, width=400)
+    # grab the frame from the video stream and resize it to have a maximum width >
+    ret, frame = vs.read()
+    if not ret:
+        break
 
+    frame = cv2.resize(frame, (400, 300))
+    (h, w) = frame.shape[:2]
+    resized_image = cv2.resize(frame, (300, 300))
+    blob = cv2.dnn.blobFromImage(resized_image, (1/127.5), (300, 300), 127.5, swa>
+    net.setInput(blob)
+    predictions = net.forward()
 
-	(h, w) = frame.shape[:2]
-	resized_image = cv2.resize(frame, (300, 300))
-	blob = cv2.dnn.blobFromImage(resized_image, (1/127.5), (300, 300), 127.5, swapRB=True)
+    # loop over the predictions
+    for i in np.arange(0, predictions.shape[2]):
+        confidence = predictions[0, 0, i, 2]
+        if confidence > args["confidence"]:
+            idx = int(predictions[0, 0, i, 1])
+            box = predictions[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+            label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
+            print("Object detected: ", label)
 
-	
-	net.setInput(blob)
-	predictions = net.forward()
+    # Press 'q' key to break the loop
+    time.sleep(1)  # Sleep for 1 second
+    key = 0  # Set a dummy value for 'key'
+    if key == ord("q"):
+        break
 
-
-	for i in np.arange(0, predictions.shape[2]):
-		confidence = predictions[0, 0, i, 2]
-		if confidence > args["confidence"]:
-			idx = int(predictions[0, 0, i, 1])
-			box = predictions[0, 0, i, 3:7] * np.array([w, h, w, h])
-			(startX, startY, endX, endY) = box.astype("int")
-
-			label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
-			print("Object detected: ", label)
-
-			cv2.rectangle(frame, (startX, startY), (endX, endY),
-				COLORS[idx], 2)
-			y = startY - 15 if startY - 15 > 15 else startY + 15
-			cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
-
-	cv2.imshow("Detected Objects", frame)
-
-	key = cv2.waitKey(1) & 0xFF
-	if key == ord("q"):
-		break
-
-	fps.update()
-
-fps.stop()
-
-
-print("[INFO] Elapsed Time: {:.2f}".format(fps.elapsed()))
-print("[INFO] Approximate FPS: {:.2f}".format(fps.fps()))
-
+# cleanup
+vs.release()
 cv2.destroyAllWindows()
-vs.stop()
+
+
